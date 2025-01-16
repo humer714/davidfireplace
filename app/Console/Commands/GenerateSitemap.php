8cf47b6f2a98c914a3016f5d2f1dfbf6a1418583
Umer <119045470+humer714@users.noreply.github.com>
2025-01-16 21:43:05 +0500

    Update Changes

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -2,13 +2,13 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Console\Command;
-use Spatie\Sitemap\Sitemap;
-use Spatie\Sitemap\Tags\Url;
-use App\Models\Service;
 use App\Models\Blog;
 use App\Models\City;
+use App\Models\Service;
 use App\Models\Projects;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+use Illuminate\Console\Command;
 
 class GenerateSitemap extends Command
 {
@@ -17,45 +17,70 @@ class GenerateSitemap extends Command
 
     public function handle()
     {
+        // Define your live domain URL here
+        $baseUrl = 'https://davidfireplacecleaningrepair.com';
+
         // Create a new sitemap instance
         $sitemap = Sitemap::create();
 
         // Add static routes
-        $sitemap->add(Url::create('/'))
-                ->add(Url::create('/about-us'))
-                ->add(Url::create('/services'))
-                ->add(Url::create('/projects'))
-                ->add(Url::create('/contact'))
-                ->add(Url::create('/locations'))
-                ->add(Url::create('/blogs'));
+        $staticRoutes = [
+            '/',
+            '/about-us',
+            '/services',
+            '/projects',
+            '/contact',
+            '/locations',
+            '/blogs',
+        ];
+
+        foreach ($staticRoutes as $route) {
+            $sitemap->add(Url::create("{$baseUrl}{$route}"));
+        }
 
         // Add dynamic routes for services
-        $services = Service::all(); // Make sure the 'slug' column exists
+        $services = Service::all(); 
         foreach ($services as $service) {
-            $sitemap->add(Url::create("/services/{$service->slug}"));
+            $sitemap->add(Url::create("{$baseUrl}/services/{$service->slug}"));
         }
 
         // Add dynamic routes for projects
-        $projects = Projects::all(); // Make sure the 'slug' column exists
+        $projects = Projects::all();
         foreach ($projects as $project) {
-            $sitemap->add(Url::create("/projects/{$project->slug}"));
+            $sitemap->add(Url::create("{$baseUrl}/projects/{$project->slug}"));
         }
 
         // Add dynamic routes for blog posts
-        $blogs = Blog::all(); // Make sure the 'slug' column exists
+        $blogs = Blog::all();
         foreach ($blogs as $blog) {
-            $sitemap->add(Url::create("/blogs/{$blog->slug}"));
+            $sitemap->add(Url::create("{$baseUrl}/blogs/{$blog->slug}"));
         }
 
         // Add dynamic routes for cities
-        $cities = City::all(); // Make sure the 'slug' column exists
+        $cities = City::all();
         foreach ($cities as $city) {
-            $sitemap->add(Url::create("/{$city->slug}"));
+            $sitemap->add(Url::create("{$baseUrl}/{$city->slug}"));
         }
 
-        // Save the sitemap to a file (public folder, for example)
+        // Save the sitemap to a file
         $sitemap->writeToFile(public_path('sitemap.xml'));
+        
+        // Modify the XML to remove <changefreq> and <priority> tags
+        $this->removeUnnecessaryTags(public_path('sitemap.xml'));
 
         $this->info('Sitemap generated successfully!');
+    }
+
+    protected function removeUnnecessaryTags($filePath)
+    {
+        // Get the content of the sitemap
+        $sitemapContent = file_get_contents($filePath);
+
+        // Remove <changefreq> and <priority> tags
+        $sitemapContent = preg_replace('/<changefreq>.*?<\/changefreq>/', '', $sitemapContent);
+        $sitemapContent = preg_replace('/<priority>.*?<\/priority>/', '', $sitemapContent);
+
+        // Write the modified content back to the file
+        file_put_contents($filePath, $sitemapContent);
     }
 }
